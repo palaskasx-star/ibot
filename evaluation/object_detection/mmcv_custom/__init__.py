e2745d8e2edec9f1d4b269b750afea549cbc623e
palaskasc <palaskasx@gmail.com>
2026-04-23 20:15:28 +0300

    Update __init__.py

--- evaluation/object_detection/mmcv_custom/__init__.py
+++ evaluation/object_detection/mmcv_custom/__init__.py
@@ -2,6 +2,6 @@
 
 #from .checkpoint import load_checkpoint
 from .layer_decay_optimizer_constructor import LayerDecayOptimizerConstructor
-from .register_backbone import VisionTransformer
+from .register_backbone import TimmViTWithFPN
 
 __all__ = ['load_checkpoint', 'LayerDecayOptimizerConstructor']
