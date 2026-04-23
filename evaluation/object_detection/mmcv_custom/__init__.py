eab56b3569b6e4d92d1d0e0df0967e6e46cb9ee6
palaskasc <palaskasx@gmail.com>
2026-04-23 20:14:20 +0300

    Update __init__.py

--- evaluation/object_detection/mmcv_custom/__init__.py
+++ evaluation/object_detection/mmcv_custom/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .checkpoint import load_checkpoint
+#from .checkpoint import load_checkpoint
 from .layer_decay_optimizer_constructor import LayerDecayOptimizerConstructor
 from .register_backbone import VisionTransformer
 
