bb78b679a36459be4cfb1324acc154036cc77ce5
palaskasc <palaskasx@gmail.com>
2026-04-24 16:06:56 +0300

    Update vit_small_giou_4conv1f_coco_3x.py

--- evaluation/object_detection/configs/cascade_rcnn/vit_small_giou_4conv1f_coco_3x.py
+++ evaluation/object_detection/configs/cascade_rcnn/vit_small_giou_4conv1f_coco_3x.py
@@ -145,7 +145,7 @@ data = dict(train=dict(pipeline=train_pipeline))
 # optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, betas=(0.9, 0.999), weight_decay=0.05,
 #                  paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
 #                                                  'norm': dict(decay_mult=0.)}))
-optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, betas=(0.9, 0.999), weight_decay=0.05,
+optimizer = dict(_delete_=True, type='AdamW', lr=0.0002, betas=(0.9, 0.999), weight_decay=0.05,
                  constructor='LayerDecayOptimizerConstructor', 
                  paramwise_cfg=dict(num_layers=12, layer_decay_rate=0.75))
 
