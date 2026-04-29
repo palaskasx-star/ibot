79cd7ccaff587047d82dca94906427c7caf6aec5
palaskasc <palaskasx@gmail.com>
2026-04-29 16:41:49 +0300

    Update vit_tiny_giou_4conv1f_coco_3x.py

--- evaluation/object_detection/configs/cascade_rcnn/vit_tiny_giou_4conv1f_coco_3x.py
+++ evaluation/object_detection/configs/cascade_rcnn/vit_tiny_giou_4conv1f_coco_3x.py
@@ -8,7 +8,7 @@ _base_ = [
 # Change 'my_custom_modules.tiny_vit' to the actual python path where 
 # you saved the @register_model code.
 custom_imports = dict(
-    imports=['my_custom_modules.tiny_eva'], 
+    imports=['custom_models.tiny_eva'], 
     allow_failed_imports=False
 )
 
